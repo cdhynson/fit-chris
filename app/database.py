aa1e46cbfb94aaa31952d06c444fe8a8f9b2c593
chynson <chynson@ucsd.edu>
2025-03-14 21:51:52 -0700

    changed databse for connections

--- app/database.py
+++ app/database.py
@@ -39,10 +39,13 @@ def get_db_connection(
     while attempt <= max_retries:
         try:
             connection = mysql.connector.connect(
-                host=os.getenv("MYSQL_HOST"),
-                user=os.getenv("MYSQL_USER"),
-                password=os.getenv("MYSQL_PASSWORD"),
-                database=os.getenv("MYSQL_DATABASE"),
+                host=os.getenv('MYSQL_HOST'),
+                port=int(os.getenv('MYSQL_PORT')),
+                user=os.getenv('MYSQL_USER'),
+                password=os.getenv('MYSQL_PASSWORD'),
+                database=os.getenv('MYSQL_DATABASE'),
+                ssl_ca=os.getenv('MYSQL_SSL_CA'),  # Path to CA certificate file
+                ssl_verify_identity=True
             )
 
             # Test the connection
