648c68f4d481498f08103ba943cd3231eb869f95
chynson <chynson@ucsd.edu>
2025-03-14 20:12:06 -0700

    test 1

--- app/app.py
+++ app/app.py
@@ -871,5 +871,5 @@ async def delete_clothing(item_id: int, request: Request):
 
 
 
-if __name__ == "__main__":
-   uvicorn.run(app="app.main:app", host="0.0.0.0", port=3306, reload=True)
+# if __name__ == "__main__":
+#    uvicorn.run(app="app.main:app", host="0.0.0.0", port=3306, reload=True)
