8c75670c3fe2bf6eab0a25a91bfec461090401dd
chynson <chynson@ucsd.edu>
2025-03-14 21:42:28 -0700

    app bottom code removed and changed yml mysql port to avian

--- app/app.py
+++ app/app.py
@@ -884,5 +884,5 @@ async def delete_clothing(item_id: int, request: Request):
 
 
 
-if __name__ == "__main__":
-   uvicorn.run(app="app.main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#    uvicorn.run(app="app.main:app", host="0.0.0.0", port=8000, reload=True)
